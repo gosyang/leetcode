c2459702f2662676af927cbd99368adc9a7acdbe
gosyang <goslingyang@gmail.com>
2017-05-30 23:59:02 +0800

    Reverse_Words_in_a_String_III

---
+++ Algorithms/557.Reverse_Words_in_a_String_III/reverse_words_in_a_string_III.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: reverse_words_in_a_string_III.py
+Author: gosyang
+Date: 2017/05/30 23:50:57
+"""
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return " ".join(map(lambda x: x[::-1], s.split(" ")))
+        
+        # others
+        # return ' '.join(x[::-1] for x in s.split(' '))
+        # return ' '.join(s.split(' ')[::-1])[::-1]
