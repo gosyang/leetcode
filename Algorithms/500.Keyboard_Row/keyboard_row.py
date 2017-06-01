c6d52f28488f59131e1bc7f58bf1c6256a4da3fa
gosyang <goslingyang@gmail.com>
2017-06-01 10:24:52 +0800

    500.Keyboard_Row/

---
+++ Algorithms/500.Keyboard_Row/keyboard_row.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: keyboard_row.py
+Author: gosyang
+Date: 2017/06/01 09:44:37
+"""
+
+
+class Solution(object):
+    def findWords(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        res = []
+        row_q = "qwertyuiop"
+        row_a = "asdfghjkl"
+        
+        for word in words:
+            row = None
+            for letter in word:
+                letter = letter.lower()
+                if letter in row_q:
+                    row_this = "q"
+                elif letter in row_a:
+                    row_this = "a"
+                else:
+                    row_this = "z"
+                if row is None:
+                    row = row_this
+                elif row != row_this:
+                    row = -1
+                    break
+            if row != -1:
+                res.append(word)
+        return res
+    
+    # 何时才能pythonic?
+    # row1 = set("qwertyuiopQWERTYUIOP")
+    # row2 = set("asdfghjklASDFGHJKL")
+    # row3 = set("zxcvbnmZXCVBNM")
+    # return [word for word in words if set(word).issubset(row1) or
+    #         set(word).issubset(row2) or
+    #         set(word).issubset(row3)]
+
---
+++ Algorithms/500.Keyboard_Row/keyboard_row_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: keyboard_row_test.py
+Author: gosyang
+Date: 2017/05/15 16:10:09
+"""
+import unittest
+
+import keyboard_row
+
+
+class KeyboardRowTestCase(unittest.TestCase):
+    def setUp(self):
+        self.solution = keyboard_row.Solution()
+
+    def tearDown(self):
+        self.solution = None
+
+    def test_example(self):
+        input = ["Hello", "Alaska", "Dad", "Peace"]
+        output = ["Alaska", "Dad"]
+        self.assertItemsEqual(self.solution.findWords(input), output)
+        
+        
+if __name__ == "__main__":
+    unittest.main()
+
+
