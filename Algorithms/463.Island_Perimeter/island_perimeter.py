76ee53e449e5c4552015c85948d5585fc37d8811
gosyang <goslingyang@gmail.com>
2017-06-04 16:28:57 +0800

    463. island perimeter

---
+++ Algorithms/463.Island_Perimeter/island_perimeter.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: island_perimeter.py
+Author: gosyang 
+Date: 2017/06/04 15:43:56
+"""
+
+
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        res = 0
+        row = len(grid)
+        if row == 0:
+            return res
+        column = len(grid[0])
+        for i in range(row):
+            for j in range(column):
+                if grid[i][j] == 0:
+                    continue
+                if j == 0:
+                    res += 1
+                elif grid[i][j - 1] == 0:
+                    res += 1
+                if j == column - 1:
+                    res += 1
+                elif grid[i][j + 1] == 0:
+                    res += 1
+                
+                if i == 0:
+                    res += 1
+                elif grid[i - 1][j] == 0:
+                    res += 1
+                if i == row - 1:
+                    res += 1
+                elif grid[i + 1][j] == 0:
+                    res += 1
+        return res
+                    
+                    
+                
+                    
+                    
---
+++ Algorithms/463.Island_Perimeter/island_perimeter_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: island_perimeter_test.py
+Author: gosyang
+Date: 2017/05/15 16:10:09
+"""
+import unittest
+
+import island_perimeter
+
+
+class IslandPerimeterTestCase(unittest.TestCase):
+    def setUp(self):
+        self.solution = island_perimeter.Solution()
+
+    def tearDown(self):
+        self.solution = None
+
+    def test_example(self):
+        grid = [[0, 1, 0, 0],
+                [1, 1, 1, 0],
+                [0, 1, 0, 0],
+                [1, 1, 0, 0]]
+
+        self.assertEqual(self.solution.islandPerimeter(grid), 16)
+    
+    def test_empty(self):
+        grid = []
+        self.assertEqual(self.solution.islandPerimeter(grid), 0)
+
+    def test_empty_empty(self):
+        grid = [[]]
+        self.assertEqual(self.solution.islandPerimeter(grid), 0)
+        
+    def test_wrong_case1(self):
+        grid = [[0],
+                [1],
+                [1]]
+        self.assertEqual(self.solution.islandPerimeter(grid), 6)
+        
+if __name__ == "__main__":
+    unittest.main()
+
+
