61d3135c16d0d26c39b21db1d8d7c84bd1c01905
gosyang <goslingyang@gmail.com>
2017-06-09 20:47:05 +0800

    104.Maximum_Depth_of_Binary_Tree/

---
+++ Algorithms/104.Maximum_Depth_of_Binary_Tree/maximum_depth_of_binary_tree.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: maximum_depth_of_binary_tree.py
+Author: gosyang 
+Date: 2017/06/09 20:41:28
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
