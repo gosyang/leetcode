20558c4bfb5b9f30a8220881197c9d2fe53ab36b
gosyang <goslingyang@gmail.com>
2017-06-11 16:01:55 +0800

    Algorithms/617.Merge_Two_Binary_Trees/

---
+++ Algorithms/617.Merge_Two_Binary_Trees/merge_two_binary_trees.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 Baidu.com, Inc. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: merge_two_binary_trees.py
+Author: baidu(baidu@baidu.com)
+Date: 2017/06/11 15:59:27
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is None and t2 is None:
+            return None
+        
+        if t1 is None and t2 is not None:
+            return t2
+        
+        if t1 is not None and t2 is None:
+            return t1
+        
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
+
