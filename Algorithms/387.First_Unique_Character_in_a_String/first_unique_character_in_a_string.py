250fc33e28f25300493747c8a9b73f6916f14b9d
shihelu <shihelu@baidudeMacBook-Air.local>
2017-06-23 16:09:22 +0800

    import collections
    class Solution(object):
        def firstUniqChar(self, s):

---
+++ Algorithms/387.First_Unique_Character_in_a_String/first_unique_character_in_a_string.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 gosyang. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: Hamming_Distance.py
+Author: gosyang
+Date: 2017/06/23 16:06:38
+"""
+
+import collections
+
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        map = collections.Counter(s)
+        for i, v in enumerate(s):
+            if map[v] == 1:
+                return i
+        return -1
