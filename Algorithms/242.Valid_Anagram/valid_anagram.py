38bc35ac8fbb44a3e237403bf8d5709db675acfd
gosyang <goslingyang@gmail.com>
2017-06-24 10:12:37 +0800

    242.Valid_Anagram

---
+++ Algorithms/242.Valid_Anagram/valid_anagram.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2017 gosyang. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File: valid_anagram.py
+Author: gosyang
+Date: 2017/06/24 16:06:38
+"""
+
+import collections
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return collections.Counter(s) == collections.Counter(t)
+        
