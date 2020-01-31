7a7b11ceed8aeb791e4fc9de86227aa5abed4c35
yanghanlin <yanghanlin@jd.com>
2020-01-31 12:40:24 +0800

    dp 91 解码的总结法

--- Algorithms/dp/91.decode-ways/decode_ways.go
+++ Algorithms/dp/91.decode-ways/decode_ways.go
@@ -8,6 +8,41 @@ modification history
 */
 /*
 DESCRIPTION
-xx
+一条包含字母 A-Z 的消息通过以下方式进行了编码：
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+给定一个只包含数字的非空字符串，请计算解码方法的总数。
 */
 package _1_decode_ways
+
+func numDecodings(s string) int {
+	if s[0] == '0' {
+		return 0
+	}
+	if len(s) == 1 {
+		return 1
+	}
+	two := 1
+	one := 1
+	for i := 1; i < len(s); i++ {
+		tmp := one
+		// 如果最后一位是0，那就不能算i-1的所有解法了，只能算i-2的
+		if s[i] == '0' {
+			one = 0
+		}
+		if s[i-1] == '1' || s[i-1] == '2' && s[i] <= '6'{
+			one += two
+		}
+		two = tmp
+	}
+	return one
+}
+
+// a. dp[i] 到第i个字符的总数
+// b. dp[i] = dp[i-1] + dp[i-2]
+// 			  1. 如果第i位是0， 则dp[i] = dp[i-2]
+// 			  2. i-1和i 不在10-26内(01-09都是非法的), dp[i] = dp[i-1]
+// c.边界条件 dp[0] = 1
--- Algorithms/dp/91.decode-ways/decode_ways_test.go
+++ Algorithms/dp/91.decode-ways/decode_ways_test.go
@@ -11,3 +11,61 @@ DESCRIPTION
 xx
 */
 package _1_decode_ways
+
+import "testing"
+
+func TestNumDecodings(t *testing.T) {
+	tests := []struct{
+		name string
+		input string
+		expect int
+	}{
+		{
+			name: "0",
+			input: "0",
+			expect: 0,
+		},
+		{
+			name: "00",
+			input: "00",
+			expect: 0,
+		},
+		{
+			name: "000",
+			input: "000",
+			expect: 0,
+		},
+		{
+			name: "101",
+			input: "101",
+			expect: 1,
+		},
+		{
+			name: "1",
+			input: "1",
+			expect: 1,
+		},
+		{
+			name: "10",
+			input: "10",
+			expect: 1,
+		},
+		{
+			name: "12",
+			input: "12",
+			expect: 2,
+		},
+		{
+			name: "226",
+			input: "226",
+			expect: 3,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			if get := numDecodings(test.input); get != test.expect {
+				t.Errorf("numDecodings(%s)=%d != %d", test.input, get, test.expect)
+			}
+		})
+	}
+}
